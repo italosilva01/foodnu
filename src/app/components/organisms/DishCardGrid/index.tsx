9ab16ce4965c3358129463ae61727490589e6fbb
Italo Silva <italo.silvan87@gmail.com>
2025-06-07 16:21:26 -0300

    Remove a altura fixa do DishCardGrid para melhorar a responsividade e garantir que o layout se ajuste corretamente em diferentes tamanhos de tela.

--- src/app/components/organisms/DishCardGrid/index.tsx
+++ src/app/components/organisms/DishCardGrid/index.tsx
@@ -32,7 +32,7 @@ export const DishCardGrid: React.FC<DishCardGridProps> = ({ dishes,
         return () => { observer.disconnect() };
     }, [hasMore, isLoading, onLoadMore]);
     return (
-        <div className="w-full h-full ld:h-[calc(100vh-200px)] xl:h-[calc(100vh)] overflow-y-auto">
+        <div className="w-full h-full ld:h-[calc(100vh-200px)] overflow-y-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-fit max-w-[1095px] lg:min-w-full mx-auto">
                 {dishes.map(({ id, name, image, price, tags, category }) => {
                     const firstTag = tags[0];
