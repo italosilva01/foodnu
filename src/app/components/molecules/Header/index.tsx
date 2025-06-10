5239127037902aae84c8cf376751a13ae7472d8a
Italo Silva <italo.silvan87@gmail.com>
2025-06-09 22:01:47 -0300

    wip: add Header component

--- src/app/components/molecules/Header/index.tsx
+++ src/app/components/molecules/Header/index.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
+import { Logo } from '@atoms/Logo'
 
 export const Header = () => {
 
     return (
-        <header className="flex flex-row px-20 items-end gap-5 mt-4">
-            <div className='p-5 pr-3 rounded-2xl bg-black flex justify-center items-center'>
-                <Image src="/assets/S..svg" alt="Logo" width={50} height={50} />
-            </div>
-            <h1 className="border-b-2 border-red-200 text-5xl h-fit">SUSHI FOOD</h1>
+        <header className="flex flex-row px-2 lg:px-20 items-end gap-5 mt-4">
+            <Logo />
+            <h1 className="border-b-2 border-red-200 text-5xl h-fit text-pretty">SUSHI FOOD</h1>
         </header>
     )
 }
