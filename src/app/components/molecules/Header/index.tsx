db17049f82c84c56b74b4394ff8bb85ae2feead1
Italo Silva <italo.silvan87@gmail.com>
2025-06-10 20:32:51 -0300

    refactor: update Header component styles for improved layout
    - adjust header max height for better responsiveness
    - change class name from 'h-fit' to 'size-fit' for consistency in styling
    - ensure TailwindCSS classes are utilized for layout and design

--- src/app/components/molecules/Header/index.tsx
+++ src/app/components/molecules/Header/index.tsx
@@ -3,9 +3,9 @@ import { Logo } from '@atoms/Logo'
 export const Header = () => {
 
     return (
-        <header className="flex flex-row px-2 lg:px-20 items-end gap-5 mt-4">
+        <header className="flex flex-row px-2 max-h-fit lg:px-20 items-end gap-5 mt-4">
             <Logo />
-            <h1 className="border-b-2 border-red-200 text-5xl h-fit text-pretty">SUSHI FOOD</h1>
+            <h1 className="border-b-2 border-red-200 text-5xl size-fit text-pretty">SUSHI FOOD</h1>
         </header>
     )
 }
