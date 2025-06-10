a98a40c7b5eff5f1ec795bb31cf3457da5287a88
Italo Silva <italo.silvan87@gmail.com>
2025-06-09 22:04:45 -0300

    wip: update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { Filter } from "@molecules/Filter";
 import { Suspense } from "react";
-import { DishesLoader } from "./components/molecules/DishesLoader";
+import { DishesLoader } from "@molecules/DishesLoader";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full !mx-auto ">
-      <div className="flex gap-4 !w-full m-4 lg:ml-0"  >
+    <div className="flex flex-col pl-3 w-full lg:p-0 !mx-auto justify-start">
+      <div className="flex gap-4 !w-full m-4 ml-0"  >
         <Filter />
       </div>
-      <Suspense fallback={<div className="flex justify-center items-center !size-full">Loading...</div>}>
+      <Suspense fallback={<div className="flex justify-center items-center !size-full w-full !mx-auto lg:min-w-full lg:w-[1095px]">Loading...</div>}>
         <DishesLoader />
       </Suspense>
     </div>
