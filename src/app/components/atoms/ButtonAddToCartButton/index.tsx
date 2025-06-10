77130e81c3879a3ee06ce5305063ae90ed967a68
Italo Silva <italo.silvan87@gmail.com>
2025-06-10 20:31:53 -0300

    wip: add AddToCartButton component
    - add AddToCarButton for add some Dish in Car
    - add styles Tailwindcss

---
+++ src/app/components/atoms/ButtonAddToCartButton/index.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+
+interface AddToCartButtonProps {
+    dishId: string,
+    className?: string
+}
+
+export const AddToCartButton = ({ dishId, className = '' }: AddToCartButtonProps) => {
+    const handleAddToCart = () => {
+        console.log('testestest')
+    }
+
+    return (
+        <Button className={`ml-auto text-white !bg-black p-5 hover:bg-gray-500 ${className}`} onClick={handleAddToCart} >
+            Adicionar ao carrinho
+        </Button >
+    )
+} 
