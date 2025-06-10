e42c7e12576f3cf7f6b035e0f697f5853c686467
Italo Silva <italo.silvan87@gmail.com>
2025-06-10 20:32:07 -0300

    wip: update Logo component to handle click navigation
    - add useRouter for navigation
    - implement handleClick function to redirect to home on click
    - adjust styles for consistent height

--- src/app/components/atoms/Logo/index.tsx
+++ src/app/components/atoms/Logo/index.tsx
@@ -1,8 +1,14 @@
+"use client"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 export const Logo = () => {
+    const router = useRouter()
+    const handleCLick = () => {
+        router.push('/')
+    }
     return (
-        <div className='p-5 pr-3 rounded-2xl bg-black flex justify-center items-center'>
+        <div className='p-5 pr-3 rounded-2xl bg-black flex justify-center items-center max-h-[5.3125rem] min-h-[5.3125rem] h-[5.3125rem]' onClick={handleCLick}>
             <Image src="/assets/S..svg" alt="Logo" width={50} height={50} />
         </div>
     )
