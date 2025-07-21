9ebdd814ac92156705f007ba2d167972f4fbc4bd
Italo Silva <italo.silvan87@gmail.com>
2025-07-21 20:51:12 -0300

    refactor: remove Filter component from Home page for simplified layout
    - eliminate the Filter component from the Home page to enhance UI clarity
    - maintain the DishesLoader for dish loading functionality
    - ensure consistent application of TailwindCSS classes for styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,6 @@ export default function Home() {
 
   return (
     <div className="flex flex-col pl-3 w-full lg:p-0 !mx-auto justify-start">
-      <div className="flex gap-4 !w-full m-4 ml-0"  >
-        <Filter />
-      </div>
       <div className="flex flex-col w-full !mx-auto ">
         <DishesLoader />
       </div>
