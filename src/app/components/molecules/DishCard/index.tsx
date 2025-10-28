661683ac92e3df457bd0babdcdcb9067f811c1de
italo silva <italo.silvan87@gmail.com>
2025-10-27 21:09:13 -0300

    feat: enhance DishCard component with add-to-cart functionality
    - integrate PlusIcon for the add-to-cart button within the DishCard
    - implement event handling to prevent click propagation and log interactions
    - adjust layout to position the add-to-cart button within the card for improved user experience

--- src/app/components/molecules/DishCard/index.tsx
+++ src/app/components/molecules/DishCard/index.tsx
@@ -3,6 +3,7 @@
 import { Card } from "@/app/components/atoms/Card";
 import { formatCurrency } from "@/app/utils/functions";
 import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 interface DishCardProps {
@@ -18,8 +19,12 @@ interface DishCardProps {
 }
 const ButtonAddToCar = () => {
     return (
-        <Button >
-            <
+        <Button className="bg-white rounded-full w-10 h-10 z-50 hover" onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            console.log('Clicou no botão');
+        }} >
+            <PlusIcon className="group-hover:text-red-500 text-gray-500" />
         </Button>
     )
 }
@@ -27,15 +32,26 @@ export const DishCard = ({ dataDish, id }: DishCardProps) => {
     const router = useRouter()
     const { image, title, price } = dataDish;
 
-    const handleClick = () => {
-        router.push(`dishes/${id}`)
+    const handleClick = (e: React.MouseEvent) => {
+        console.log('Clicou no card');
+        // router.push(`dishes/${id}`)
+        e.stopPropagation();
+        e.preventDefault();
     }
 
     return (
-        <Card.Root onClick={handleClick} className=" max-w-[7.625rem]">
+        <Card.Root onClick={(e) => handleClick(e)} className=" max-w-[7.625rem]">
             <Card.Content className="">
                 <div className="relative h-fit border-1 border-rose-50">
                     <Card.Image image={image} width={122} height={61} />
+                    <div className="absolute bottom-[5px] right-[5px]">
+                        <div className="relative z-50 " onClick={e => {
+                            // Impede que o click do Card.Root dispare
+                            e.stopPropagation();
+                        }} >
+                            <ButtonAddToCar />
+                        </div>
+                    </div>
                 </div>
                 <p className="text-lg text-black">
                     {formatCurrency(price)}
