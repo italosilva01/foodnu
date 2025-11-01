901e5a2783decd862a1bb5b5633faf62a2957a8c
italo silva <italo.silvan87@gmail.com>
2025-11-01 13:09:51 -0300

    feat: add user-event library and enhance ButtonMinAddToCar tests
    - add @testing-library/user-event to package.json and package-lock.json for improved user interaction testing
    - refactor ButtonMinAddToCar component to accept a disabled prop
    - enhance test cases for ButtonMinAddToCar to include button click functionality and disabled state verification
    - implement cleanup in tests to ensure proper rendering and state management

--- src/app/components/atoms/ButtonMinAddToCar/ButtonMinAddToCar.spec.tsx
+++ src/app/components/atoms/ButtonMinAddToCar/ButtonMinAddToCar.spec.tsx
@@ -1,10 +1,48 @@
-import { render, screen, } from '@testing-library/react'
+import { cleanup, render, screen, } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { ButtonMinAddToCar } from './index'
+import '@testing-library/jest-dom'
 
 describe('ButtonMinAddToCar', () => {
+
+    beforeEach(() => {
+        return render(<ButtonMinAddToCar />)
+    })
+
+    afterEach(() => { cleanup() })
+
     it('should render the button', () => {
-        render(<ButtonMinAddToCar />)
         expect(screen.getByRole('button')).toBeDefined()
     })
 
+    it('should render the plusIcon', () => {
+        const plusIcon = screen.getByRole('svg');
+        expect(plusIcon).toBeDefined()
+    })
+
+    it('should call the onClick function when the button is clicked', async () => {
+        const user = userEvent.setup()
+        const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => { })
+
+        render(<ButtonMinAddToCar />)
+
+        const button = screen.getByRole('button')
+        await user.click(button)
+
+        screen.debug(button)
+
+        expect(consoleSpy).toHaveBeenCalledWith('Adicionar ao carrinho')
+    })
+
+    describe("disabled button", () => {
+        beforeEach(() => {
+            cleanup()
+        })
+
+        it('should be disabled button', async () => {
+            render(<ButtonMinAddToCar disabled />)
+            const button = screen.getByRole('button')
+            expect(button).toBeDisabled()
+        })
+    })
 })
